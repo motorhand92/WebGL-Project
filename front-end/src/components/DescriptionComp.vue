4c0ffd19c54e6c12024598af29fdfcb8aad9c866
Mert Kuldemir <mertkuldemir92@gmail.com>
2024-10-20 14:12:06 +0300

    animations

--- front-end/src/components/DescriptionComp.vue
+++ front-end/src/components/DescriptionComp.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="product-name">
     <h3>Product Name:</h3>
-    <h3 v-if="product" class="product-text">{{ this.product.id }}</h3>
+    <div>
+      <h3 v-if="product.id === 'Big Date'" class="product-text">{{ this.product.id }}</h3>
+      <h3 v-if="product.id === 'Chrono'" class="product-text">{{ this.product.id }}</h3>
+      <h3 v-if="product.id === 'Power Reserve'" class="product-text">{{ this.product.id }}</h3>
+    </div>
   </div>
-  <div v-if="product">
+  <div>
     <h5>Description:</h5>
-    <div v-for="desc in this.product.description" :key="desc">
-      <li>{{ desc }}</li>
+    <div v-if="product.id === 'Big Date'">
+      <div v-for="desc in this.product.description" :key="desc">
+        <li>{{ desc }}</li>
+      </div>
+    </div>
+    <div v-if="product.id === 'Chrono'">
+      <div v-for="desc in this.product.description" :key="desc">
+        <li>{{ desc }}</li>
+      </div>
+    </div>
+    <div v-if="product.id === 'Power Reserve'">
+      <div v-for="desc in this.product.description" :key="desc">
+        <li>{{ desc }}</li>
+      </div>
     </div>
   </div>
 </template>
